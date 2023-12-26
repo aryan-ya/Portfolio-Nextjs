274076082fc46aa5637f86a73c214f909478e960
Aryan Kumar Yadav <ay3978943@gmail.com>
2023-12-26 08:37:23 +0530

    portfolio

--- portfolio-website/src/app/components/Navbar.jsx
+++ portfolio-website/src/app/components/Navbar.jsx
@@ -1,5 +1,8 @@
+"use client";
 import Link from 'next/link'
-import React from 'react'
+import React, {useState} from 'react'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
 
  import NavLink from "./NavLink";   
 
@@ -26,11 +29,25 @@ const navlinks  = [
 ] 
 
 const Navbar = () => {
+  const [navbarOpen , setNavbarOpen] = useState(false)
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4">
         <Link href={"/"} className="text-2xl md:text-5xl  text-white font-semibold">LOGO</Link>
-        <div className="menu block md:w-auto" id="navbar">
+
+        <div className='mobile-menu block md:hidden'>
+          {
+            navbarOpen ?  (
+             <button className='flex items-center px-3 py-2 border rounded border-slate-200  text-slate-200 hover:text-white hover:border-white'>
+              <Bars3Icon className="h-5 w-5" />
+             </button>
+            ) : (
+              <button className='flex items-center px-3 py-2 border rounded border-slate-200  text-slate-200 hover:text-white hover:border-white'>
+              <XMarkIcon className="h-5 w-5" />
+             </button>
+            )}
+        </div>
+        <div className="menu hidden block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row  md:space-x-8 mt-0">
          
        
